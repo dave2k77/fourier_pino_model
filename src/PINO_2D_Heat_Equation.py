06cc7743abdb0f92a70efc8f5d7a2634b53d9ef4
Davian Chin <davianc@gmail.com>
2023-03-21 06:19:58 +0000

    updated dataset_builder.py

--- src/PINO_2D_Heat_Equation.py
+++ src/PINO_2D_Heat_Equation.py
@@ -20,7 +20,20 @@ class PINO_2D_Heat_Equation(nn.Module):
 
 if __name__ == "__main__":
 
+    # Initilise PINO 2D Heat Equation Model
     model = PINO_2D_Heat_Equation()
-    data = None # Load your data here
-    loss_history = train(model, data, epochs=100, learning_rate=1e-3, physics_loss_coefficient=1e-4)
+
+    # Load training and test data
+    data = HeatmapPDEDataset(heatmap_folder, pde_solution_folder, transform=ToTensor())
+    train_dataset, test_dataset = split_data(dataset)
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=4)
+
+    # Set hyperparameters
+    lr = 0.001 # options: 0.001, 0.005, 0.01, 0.05, 0.1
+    num_epochs = 100 # options: 100, 250, 500
+    physics_loss_coefficient = 0.001 # options: 0.01, 0.1, 1.0, 2.0
+    optimiser = optim.SGD(model.parameters(), lr=lr)
+
+    loss_history = train(model, loss_function, optimiser, train_loader, test_loader, num_epochs)
     plot_loss(loss_history)
--- src/preprocess.py
+++ src/preprocess.py
@@ -2,21 +2,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
-
-def preprocess_data(raw_input_data, device='cpu'):
-    """
-    This function is a helper function that formats and reshapes the raw input data into a format 
-    that can be used by the Fourier Transform Layer.
-    """
-
-    # Convert input data to a PyTorch tensor
-    formatted_input_data_tensor = torch.tensor(raw_input_data, dtype=torch.float32, device=device)
-
-    # Reshape
-    formatted_input_data_tensor = formatted_input_data_tensor.unsqueeze(0).unsqueeze(-1)
-
-    return formatted_input_data_tensor
-
+from torch.autograd import Variable
+from dataset_builder import *
 
 def fourier_derivative_2d(input, axis=0):
     assert input.dim() == 2, "The input must be a 2D tensor."
@@ -42,11 +29,10 @@ def fourier_derivative_2d(input, axis=0):
     return derivative
 
 
-def loss_function(output, target, initial_condition, physics_loss_coefficient):
+def loss_function(output, target, physics_loss_coefficient):
     operator_loss = nn.MSELoss()(output, target)
-    initial_condition_loss = torch.norm(initial_condition, p="2")
     physics_loss = energy_conservation_loss(output, target)
-    loss = operator_loss + initial_condition_loss + physics_loss_coefficient * physics_loss
+    loss = operator_loss + physics_loss_coefficient * physics_loss
     return loss
 
 
@@ -78,17 +64,44 @@ def plot_loss(loss_history):
     plt.show()
 
 
-def train(model, data, epochs, learning_rate, physics_loss_coefficient):
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-    loss_history = []
-    for epoch in range(epochs):
-        optimizer.zero_grad()
-        input_data, initial_condition, target = preprocess_data(data)
-        output = model(input_data)
-        loss = loss_function(output, target, initial_condition, physics_loss_coefficient)
-        loss.backward()
-        optimizer.step()
-        loss_history.append(loss.item())
-        if epoch % 10 == 0:
-            print(f'Epoch: {epoch}, Loss: {loss.item()}')
-    return loss_history
+def train(model, loss_fn, optimizer, train_loader, test_loader, num_epochs, physics_loss_coefficient=0.001):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+
+        for i, (heatmaps, pde_solutions) in enumerate(train_loader):
+            heatmaps = heatmaps.to(device)
+            pde_solutions = pde_solutions.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model(heatmaps)
+            loss_fn = loss_function(outputs, pde_solutions, physics_loss_coefficient)
+            loss_fn.backward()
+            optimizer.step()
+
+            running_loss += loss_fn.item()
+
+        epoch_loss = running_loss / (i + 1)
+        print(f"Epoch [{epoch+1}/{num_epochs}] - Loss: {epoch_loss:.4f}")
+
+        # Evaluate on the test dataset
+        model.eval()
+        test_loss = 0.0
+        with torch.no_grad():
+            for i, (heatmaps, pde_solutions) in enumerate(test_loader):
+                heatmaps = heatmaps.to(device)
+                pde_solutions = pde_solutions.to(device)
+
+                outputs = model(heatmaps)
+                loss = loss_function(outputs, pde_solutions, physics_loss_coefficient)
+
+                test_loss += loss.item()
+
+        test_epoch_loss = test_loss / (i + 1)
+        print(f"Test Loss: {test_epoch_loss:.4f}")
+
+    return model
